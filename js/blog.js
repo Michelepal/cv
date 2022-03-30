27a9212138a3f20d1f73b930ea40b94ee3a6dcee
Michelepal <mikaelquasimodus@gmail.com>
2022-03-30 11:10:21 +0200

    fix

---
+++ js/blog.js
@@ -0,0 +1,18 @@
+
+function sceltaCategoria(n) {
+    let cat1 = document.getElementById("cat1");
+    let cat2 = document.getElementById("cat2");
+    let cat3 = document.getElementById("cat3");
+    let cat4 = document.getElementById("cat4");
+    let cat5 = document.getElementById("cat5");
+    let btn = document.getElementsByClassName("scelta");
+    switch (n) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        default:
+
+    }
+}
